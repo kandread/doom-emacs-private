bdeef0368d897ce937e45b1f6989e34a53a20ab1
Kostas Andreadis <kandread@jpl.nasa.gov>
2018-04-24 06:40:16 -0700

    fixed incorrect key map in latex bindings

--- bindings/latex.el
+++ bindings/latex.el
@@ -1,10 +1,10 @@
 ;;;  -*- lexical-binding: t; -*-
 
 ;; auctex keybindings
-(map! (:map latex-mode)
-      :localleader
-      ;; editing
-      :n "e" #'LaTeX-environment
-      :n "c" #'LaTeX-close-environment
-      ;; build commands
-      :n "a" #'TeX-command-run-all)
+(map! (:map (LaTeX-mode-map)
+          :localleader
+          ;; editing
+          :n "e" #'LaTeX-environment
+          :n "c" #'LaTeX-close-environment
+          ;; build commands
+          :n "a" #'TeX-command-run-all))
